abc11a24458856038928d0680bfac32e477da46d
Rafał <rafal44334@gmail.com>
2023-05-26 02:29:57 +0200

    update

--- contracts/src/mechanics.sol
+++ contracts/src/mechanics.sol
@@ -2,7 +2,6 @@
 pragma solidity >=0.8.18;
 
 import "./main.sol";
-// import "https://github.com/smartcontractkit/chainlink/blob/master/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract XamMechanics {
@@ -18,7 +17,12 @@ contract XamMechanics {
     }
 
     /**
-     * Returns the latest price
+     * Events of the contract
+     */
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+    /**
+     * Returns the latest price based on Chainlink nodes network
      */
     function getLatestPrice() public view returns (int) {
         (
@@ -29,6 +33,17 @@ contract XamMechanics {
             /* uint80 answeredInRound */
         ) = priceFeed.latestRoundData();
         return price;
+    }
+
+    /**
+     * @notice Will cause a certain amount `_betValue` of coins bet.
+     * @param _betValue The bet value in XAM tokens.
+     * @param _betDirection The prediction of price: -1 short (decrease); 0 stays the same; 1 long (increase).
+     */
+    function placeBet(uint256 _betValue, int8 _betDirection) private returns (bool success) {
+        require(xam.balances[msg.sender] >= _betValue, "Not enough XAM to place a bet.");
+        require(_betDirection >= -1 && _betDirection <= 1, "Incorrect bet direction, must be: -1 for short, 0 or 1 for long.");
+        
     }
 
 }
