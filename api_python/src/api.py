018bf06e02f2fea30a7f09a1db0b3d1392a14ab5
Rafał <rafal44334@gmail.com>
2023-06-22 01:58:07 +0200

    Python API bug fixes

--- api_python/src/api.py
+++ api_python/src/api.py
@@ -4,8 +4,8 @@ from web3 import Web3
 from web3.gas_strategies.rpc import rpc_gas_price_strategy
 
 # Replace the RPC endpoint with the appropriate one for the Polygon network you want to connect to
-# w3 = Web3(Web3.HTTPProvider('https://rpc-mainnet.maticvigil.com'))
-w3 = Web3(Web3.HTTPProvider('https://polygon-mumbai-bor.publicnode.com'))
+# w3 = Web3(Web3.HTTPProvider('https://rpc-mumbai.maticvigil.com/'))
+w3 = Web3(Web3.HTTPProvider((config('RPC'))))
 
 # Check connection status
 print(w3.net.version)
@@ -34,37 +34,40 @@ def getBalance(_address):
 def getTotalSupply():
     return contract.functions.totalSupply().call()
 
-# def setSenderGas(_address, _gas):
-#     return {
-#         'from': _address,
-#         'gas': _gas,
-#         'gasPrice': w3.to_wei('10', 'gwei'),
-#         'nonce': w3.eth.get_transaction_count(_address),
-#         }
+def setSenderGas(_address, _gas):
+    return {
+        'from': _address,
+        'gas': _gas,
+        'gasPrice': w3.to_wei('10', 'gwei'),
+        'nonce': w3.eth.get_transaction_count(_address),
+        }
 
-# def transferTokens(_recipient, _amount, _sender, _gas=1000000):
-#     return contract.functions.transfer(_recipient, _amount).build_transaction(setSenderGas(_sender, _gas))
+def transferTokens(_recipient, _amount, _sender, _gas=1000000):
+    return contract.functions.transfer(_recipient, _amount).build_transaction(setSenderGas(_sender, _gas))
 
-# def sendTransaction(_transaction, _private_key):
-#     # Sign the transaction
-#     signed_txn = w3.eth.account.sign_transaction(_transaction, _private_key)
+def sendTransaction(_transaction, _private_key):
+    # Sign the transaction
+    signed_txn = w3.eth.account.sign_transaction(_transaction, _private_key)
 
-#     # Send the transaction
-#     tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+    # Send the transaction
+    tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
 
-#     receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
-#     print(receipt)
+    # Wait for a transaction to be mined and obtain its receipt
+    #   Note that wait_for_transaction_receipt is a blocking function, 
+    #   meaning it will pause the execution of your code until the transaction is mined and the receipt is obtained.
+    receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+    # print(receipt)
+    return True, receipt
 
-print('balance owner: ', getBalance(owner_address))
 
-w3.eth.set_gas_price_strategy(rpc_gas_price_strategy)
-chain_id = w3.eth.chain_id
-estimate = w3.eth.estimate_gas({'to': second_address, 'from': owner_address, 'value': 100})
 
-transaction = contract.functions.transfer(second_address, 100)
-unsigned_txn = transaction.build_transaction({ 'gas' : estimate, 'nonce' : w3.eth.get_transaction_count(owner_address) })
-signed_tx = w3.eth.account.sign_transaction(unsigned_txn, owner_pvkey)
-txn_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
-# print(f'Transaction successful with hash: { txn_hash.transactionHash.hex() }')
 
-getBalance(second_address)
+
+# ##################
+# #### EXAMPLES ####
+# ##################
+# # Transfer
+# print('balance owner: ', getBalance(owner_address))
+# unsigned_txn = transferTokens(second_address, 10000, owner_address, 1000000)
+# sendTransaction(unsigned_txn, owner_pvkey)
+# print('balane second_address: ', getBalance(second_address))
